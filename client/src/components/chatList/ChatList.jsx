72cc180f643f678adf4fa473e535a97ad2b47732
evelioexcellenta <velioexcel@gmail.com>
2024-10-27 19:35:07 +0700

    fix userchats typo add path and url to index.js

--- client/src/components/chatList/ChatList.jsx
+++ client/src/components/chatList/ChatList.jsx
@@ -6,7 +6,7 @@ const ChatList = () => {
   const { isPending, error, data } = useQuery({
     queryKey: ["userChats"],
     queryFn: () =>
-      fetch(`${import.meta.env.VITE_API_URL}/api/userChats`, {
+      fetch(`${import.meta.env.VITE_API_URL}/api/userchats`, {
         credentials: "include",
       }).then((res) => res.json()),
   });
