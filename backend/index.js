925bbe82c73c91654ecf24ef1a10d2a40523e290
evelioexcellenta <velioexcel@gmail.com>
2024-10-28 08:03:57 +0700

    another testing

--- backend/index.js
+++ backend/index.js
@@ -137,7 +137,7 @@ app.post("api/chats", ClerkExpressRequireAuth(), async (req, res) => {
       });
       await newUserChats.save();
     } else {
-      // if exist, push the chat to existing array
+      // if exist, push the chat to existing arrayy
       await UserChats.updateOne(
         {
           userId: userId,
